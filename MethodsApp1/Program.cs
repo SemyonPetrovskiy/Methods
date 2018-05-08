7632993e2e87f056202d9be4aadf02c5df36bcc7
semenpetrovskiy88 <semenpetrovskiy88@gmail.com>
2018-05-08 17:57:25 +0300

    Добавьте файлы проекта.

---
+++ MethodsApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = Sum();
+            Console.WriteLine(c);
+            string s = Hello();
+            Method1();
+            Method2();
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+
+        static string Hello()
+        {
+            return "Hello world";
+        }
+
+        static int Sum()
+        {
+            int x = 2;
+            int y = 3;
+            int z = x + y; // 5
+            return z;
+        }
+
+        static void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+
+        static void Method2()
+        {
+            Console.WriteLine("Method2");
+        }
+    }
+}
